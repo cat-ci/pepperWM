2fd0f16323622cca5ddf7f58b9250d6c5a8af90b
catci <git@catci.net>
2025-12-06 01:12:26 +0000

    pepperctl helper added

--- control_window.cpp
+++ control_window.cpp
@@ -1,64 +1,69 @@
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
 #include <iostream>
+#include <map>
+#include <string>
 #include <vector>
 #include <cstring>
 #include <cstdlib>
 
-
 std::vector<Window> getAllWindows(Display* dpy) {
-    std::vector<Window> wins;
-    Atom clientList = XInternAtom(dpy, "_NET_CLIENT_LIST", True);
+    Atom listAtom = XInternAtom(dpy, "_NET_CLIENT_LIST", True);
+    std::vector<Window> list;
+    if (listAtom == None) return list;
+
     Atom type;
     int fmt;
-    unsigned long nitems, bytesAfter;
+    unsigned long nItems, bytesAfter;
     unsigned char* prop = nullptr;
 
-    if (XGetWindowProperty(dpy, DefaultRootWindow(dpy), clientList, 0, (~0L),
-        False, XA_WINDOW, &type, &fmt, &nitems, &bytesAfter,
-        &prop) == Success && prop) {
+    if (XGetWindowProperty(dpy, DefaultRootWindow(dpy), listAtom, 0, (~0L),
+                           False, XA_WINDOW, &type, &fmt, &nItems, &bytesAfter,
+                           &prop) == Success && prop) {
         Window* arr = reinterpret_cast<Window*>(prop);
-    for (unsigned long i = 0; i < nitems; ++i)
-        wins.push_back(arr[i]);
+        for (unsigned long i = 0; i < nItems; ++i)
+            list.push_back(arr[i]);
         XFree(prop);
-        }
-        return wins;
+    }
+    return list;
 }
 
-
 int getPID(Display* dpy, Window w) {
-    Atom atom = XInternAtom(dpy, "_NET_WM_PID", True);
-    Atom type;
-    int fmt;
-    unsigned long nitems, bytesAfter;
+    Atom atomPID = XInternAtom(dpy, "_NET_WM_PID", True);
+    if (atomPID == None) return -1;
+
+    Atom actualType;
+    int actualFormat;
+    unsigned long nItems, bytesAfter;
     unsigned char* prop = nullptr;
     int pid = -1;
 
-    if (XGetWindowProperty(dpy, w, atom, 0, 1, False, XA_CARDINAL, &type, &fmt,
-        &nitems, &bytesAfter, &prop) == Success &&
+    if (XGetWindowProperty(dpy, w, atomPID, 0, 1, False, XA_CARDINAL,
+                           &actualType, &actualFormat, &nItems, &bytesAfter,
+                           &prop) == Success &&
         prop) {
-        if (nitems > 0)
+        if (nItems > 0)
             pid = *((unsigned long*)prop);
         XFree(prop);
-        }
-        return pid;
+    }
+    return pid;
 }
-
 
 bool getFrameExtents(Display* dpy, Window w,
                      int& left, int& right, int& top, int& bottom) {
     Atom atom = XInternAtom(dpy, "_NET_FRAME_EXTENTS", True);
     if (atom == None) return false;
+
     Atom type;
     int fmt;
-    unsigned long nitems, bytesAfter;
+    unsigned long nItems, bytesAfter;
     unsigned char* prop = nullptr;
 
     if (XGetWindowProperty(dpy, w, atom, 0, 4, False, XA_CARDINAL, &type, &fmt,
-        &nitems, &bytesAfter, &prop) != Success || !prop)
+                           &nItems, &bytesAfter, &prop) != Success || !prop)
         return false;
 
-    if (nitems >= 4) {
+    if (nItems >= 4) {
         unsigned long* vals = reinterpret_cast<unsigned long*>(prop);
         left = vals[0];
         right = vals[1];
@@ -67,113 +72,132 @@ bool getFrameExtents(Display* dpy, Window w,
     }
     XFree(prop);
     return true;
-                     }
-
-                     void clearWindowStates(Display* dpy, Window w) {
-                         Atom wmState = XInternAtom(dpy, "_NET_WM_STATE", False);
-                         Atom fs = XInternAtom(dpy, "_NET_WM_STATE_FULLSCREEN", False);
-                         Atom maxH = XInternAtom(dpy, "_NET_WM_STATE_MAXIMIZED_HORZ", False);
-                         Atom maxV = XInternAtom(dpy, "_NET_WM_STATE_MAXIMIZED_VERT", False);
-
-                         auto sendState = [&](Atom stateAtom) {
-                             XEvent e;
-                             std::memset(&e, 0, sizeof(e));
-                             e.xclient.type = ClientMessage;
-                             e.xclient.message_type = wmState;
-                             e.xclient.display = dpy;
-                             e.xclient.window = w;
-                             e.xclient.format = 32;
-                             e.xclient.data.l[0] = 0;
-                             e.xclient.data.l[1] = stateAtom;
-                             e.xclient.data.l[2] = 0;
-                             e.xclient.data.l[3] = 1;
-                             e.xclient.data.l[4] = 0;
-
-                             XSendEvent(dpy, DefaultRootWindow(dpy), False,
-                                        SubstructureRedirectMask | SubstructureNotifyMask, &e);
-                         };
-
-                         sendState(fs);
-                         sendState(maxH);
-                         sendState(maxV);
-                         XFlush(dpy);
-                     }
+}
 
 
-                     void sendMoveResize(Display* dpy, Window w,
-                                         int x, int y, int width, int height) {
-                         Atom moveresize = XInternAtom(dpy, "_NET_MOVERESIZE_WINDOW", False);
-                         if (moveresize == None) {
-                             XMoveResizeWindow(dpy, w, x, y, width, height);
-                             XFlush(dpy);
-                             return;
-                         }
+void clearWindowStates(Display* dpy, Window w) {
+    Atom wmState = XInternAtom(dpy, "_NET_WM_STATE", False);
+    Atom fs = XInternAtom(dpy, "_NET_WM_STATE_FULLSCREEN", False);
+    Atom maxH = XInternAtom(dpy, "_NET_WM_STATE_MAXIMIZED_HORZ", False);
+    Atom maxV = XInternAtom(dpy, "_NET_WM_STATE_MAXIMIZED_VERT", False);
 
-                         XEvent e;
-                         std::memset(&e, 0, sizeof(e));
-                         e.xclient.type = ClientMessage;
-                         e.xclient.send_event = True;
-                         e.xclient.display = dpy;
-                         e.xclient.window = w;
-                         e.xclient.message_type = moveresize;
-                         e.xclient.format = 32;
-                         e.xclient.data.l[0] = (1 << 8) | (1 << 9) | (1 << 10) | (1 << 11);
-                         e.xclient.data.l[1] = x;
-                         e.xclient.data.l[2] = y;
-                         e.xclient.data.l[3] = width;
-                         e.xclient.data.l[4] = height;
+    auto removeState = [&](Atom stateAtom) {
+        XEvent e;
+        std::memset(&e, 0, sizeof(e));
+        e.xclient.type = ClientMessage;
+        e.xclient.message_type = wmState;
+        e.xclient.display = dpy;
+        e.xclient.window = w;
+        e.xclient.format = 32;
+        e.xclient.data.l[0] = 0;  
+        e.xclient.data.l[1] = stateAtom;
+        e.xclient.data.l[2] = 0;
+        e.xclient.data.l[3] = 1;  
+        e.xclient.data.l[4] = 0;
 
-                         XSendEvent(dpy, DefaultRootWindow(dpy), False,
-                                    SubstructureRedirectMask | SubstructureNotifyMask, &e);
-                         XFlush(dpy);
-                                         }
+        XSendEvent(dpy, DefaultRootWindow(dpy), False,
+                   SubstructureRedirectMask | SubstructureNotifyMask, &e);
+    };
 
-                                         int main(int argc, char* argv[]) {
-                                             if (argc != 6) {
-                                                 std::cerr << "Usage: " << argv[0]
-                                                 << " <PID> <X> <Y> <WIDTH> <HEIGHT>\n";
-                                                 return 1;
-                                             }
+    removeState(fs);
+    removeState(maxH);
+    removeState(maxV);
+    XFlush(dpy);
+}
 
-                                             int targetPID = std::atoi(argv[1]);
-                                             int x = std::atoi(argv[2]);
-                                             int y = std::atoi(argv[3]);
-                                             int width = std::atoi(argv[4]);
-                                             int height = std::atoi(argv[5]);
+void sendMoveResize(Display* dpy, Window w,
+                    int x, int y, int width, int height) {
+    Atom moveresize = XInternAtom(dpy, "_NET_MOVERESIZE_WINDOW", False);
+    if (moveresize == None) {
+        XMoveResizeWindow(dpy, w, x, y, width, height);
+        XFlush(dpy);
+        return;
+    }
 
-                                             Display* dpy = XOpenDisplay(nullptr);
-                                             if (!dpy) {
-                                                 std::cerr << "Cannot open X display\n";
-                                                 return 2;
-                                             }
+    XEvent e;
+    std::memset(&e, 0, sizeof(e));
+    e.xclient.type = ClientMessage;
+    e.xclient.send_event = True;
+    e.xclient.display = dpy;
+    e.xclient.window = w;
+    e.xclient.message_type = moveresize;
+    e.xclient.format = 32;
+    e.xclient.data.l[0] =
+        (1 << 8) | (1 << 9) | (1 << 10) | (1 << 11);  
+    e.xclient.data.l[1] = x;
+    e.xclient.data.l[2] = y;
+    e.xclient.data.l[3] = width;
+    e.xclient.data.l[4] = height;
 
-                                             auto windows = getAllWindows(dpy);
-                                             bool found = false;
+    XSendEvent(dpy, DefaultRootWindow(dpy), False,
+               SubstructureRedirectMask | SubstructureNotifyMask, &e);
+    XFlush(dpy);
+}
 
-                                             for (auto w : windows) {
-                                                 int pid = getPID(dpy, w);
-                                                 if (pid == targetPID) {
-                                                     found = true;
-                                                     clearWindowStates(dpy, w);
 
-                                                     int left = 0, right = 0, top = 0, bottom = 0;
-                                                     if (getFrameExtents(dpy, w, left, right, top, bottom)) {
-                                                         std::cout << "Frame extents: L=" << left << " R=" << right
-                                                         << " T=" << top << " B=" << bottom << "\n";
+int main(int argc, char* argv[]) {
+    if (argc != 6) {
+        std::cerr << "Usage: " << argv[0]
+                  << " <PID[@UID]> <X> <Y> <WIDTH> <HEIGHT>\n";
+        return 1;
+    }
 
-                                                         x += left;
-                                                         y += top;
-                                                         width -= (left + right);
-                                                         height -= (top + bottom);
-                                                     }
+    std::string arg = argv[1];
+    int basePID = 0;
+    int uid = 1;  
 
-                                                     sendMoveResize(dpy, w, x, y, width, height);
-                                                 }
-                                             }
+    size_t sep = arg.find('@');
+    if (sep != std::string::npos) {
+        basePID = std::stoi(arg.substr(0, sep));
+        uid = std::stoi(arg.substr(sep + 1));
+    } else {
+        basePID = std::stoi(arg);
+    }
 
-                                             if (!found)
-                                                 std::cerr << "No window found with PID " << targetPID << "\n";
+    int x = std::atoi(argv[2]);
+    int y = std::atoi(argv[3]);
+    int width = std::atoi(argv[4]);
+    int height = std::atoi(argv[5]);
 
-                                             XCloseDisplay(dpy);
-                                             return 0;
-                                         }
+    Display* dpy = XOpenDisplay(nullptr);
+    if (!dpy) {
+        std::cerr << "Error: cannot open X display.\n";
+        return 2;
+    }
+
+    auto windows = getAllWindows(dpy);
+    bool found = false;
+    std::map<int, int> pidCounter;
+
+    for (auto w : windows) {
+        int pid = getPID(dpy, w);
+        if (pid == -1) continue;
+        pidCounter[pid]++;
+
+        if (pid == basePID && pidCounter[pid] == uid) {
+            found = true;
+
+            clearWindowStates(dpy, w);
+
+            int left = 0, right = 0, top = 0, bottom = 0;
+            if (getFrameExtents(dpy, w, left, right, top, bottom)) {
+                std::cout << "Frame extents: top=" << top << " left=" << left
+                          << " right=" << right << " bottom=" << bottom << "\n";
+
+                x += left;
+                y += top;
+                width -= (left + right);
+                height -= (top + bottom);
+            }
+
+            sendMoveResize(dpy, w, x, y, width, height);
+            break;
+        }
+    }
+
+    if (!found)
+        std::cerr << "No window found with ID " << arg << "\n";
+
+    XCloseDisplay(dpy);
+    return 0;
+}
--- list_windows.cpp
+++ list_windows.cpp
@@ -1,183 +1,177 @@
+
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
 #include <unistd.h>
-#include <cstring>
-#include <string>
-#include <vector>
 #include <fstream>
 #include <iostream>
-#include <sstream>
-#include <jsoncpp/json/json.h>
+#include <map>
+#include <string>
+#include <vector>
+#include <json/json.h>
 
-std::string getTextProperty(Display *dpy, Window win, const char *atomName) {
+std::string getTextProperty(Display* dpy, Window win, const char* atomName) {
     Atom atom = XInternAtom(dpy, atomName, True);
-    if (atom == None)
-        return "";
+    if (atom == None) return "";
+
     Atom actualType;
     int actualFormat;
-    unsigned long nitems, bytesAfter;
-    unsigned char *prop = nullptr;
-
-    int status = XGetWindowProperty(dpy, win, atom, 0, (~0L), False,
-                                    AnyPropertyType, &actualType, &actualFormat,
-                                    &nitems, &bytesAfter, &prop);
+    unsigned long nItems, bytesAfter;
+    unsigned char* prop = nullptr;
     std::string result;
-    if (status == Success && prop && nitems > 0) {
-        result.assign(reinterpret_cast<char *>(prop),
-                      reinterpret_cast<char *>(prop) + nitems);
+
+    if (XGetWindowProperty(dpy, win, atom, 0, (~0L), False, AnyPropertyType,
+                           &actualType, &actualFormat, &nItems, &bytesAfter,
+                           &prop) == Success &&
+        prop && nItems > 0) {
+        result.assign(reinterpret_cast<char*>(prop),
+                      reinterpret_cast<char*>(prop) + nItems);
     }
     if (prop)
         XFree(prop);
     return result;
 }
 
-bool isNormalWindow(Display *dpy, Window win) {
+
+bool isNormalWindow(Display* dpy, Window win) {
     Atom atomType = XInternAtom(dpy, "_NET_WM_WINDOW_TYPE", True);
     Atom atomNormal = XInternAtom(dpy, "_NET_WM_WINDOW_TYPE_NORMAL", False);
-    if (atomType == None)
-        return false;
+    if (atomType == None) return false;
 
     Atom actualType;
     int actualFormat;
-    unsigned long nitems, bytesAfter;
-    unsigned char *prop = nullptr;
+    unsigned long nItems, bytesAfter;
+    unsigned char* prop = nullptr;
+    bool normal = false;
 
     if (XGetWindowProperty(dpy, win, atomType, 0, (~0L), False, XA_ATOM,
-        &actualType, &actualFormat, &nitems, &bytesAfter,
-        &prop) != Success) {
-        return false;
-        }
-
-        bool normal = false;
-    if (prop) {
-        Atom *atoms = reinterpret_cast<Atom *>(prop);
-        for (unsigned long i = 0; i < nitems; ++i) {
-            if (atoms[i] == atomNormal) {
+                           &actualType, &actualFormat, &nItems, &bytesAfter,
+                           &prop) == Success &&
+        prop) {
+        Atom* atoms = reinterpret_cast<Atom*>(prop);
+        for (unsigned long i = 0; i < nItems; ++i)
+            if (atoms[i] == atomNormal)
                 normal = true;
-                break;
-            }
-        }
         XFree(prop);
     }
     return normal;
 }
 
-int getPID(Display *dpy, Window win) {
+
+int getPID(Display* dpy, Window win) {
     Atom atomPID = XInternAtom(dpy, "_NET_WM_PID", True);
-    if (atomPID == None)
-        return -1;
+    if (atomPID == None) return -1;
 
     Atom actualType;
     int actualFormat;
-    unsigned long nitems, bytesAfter;
-    unsigned char *prop = nullptr;
+    unsigned long nItems, bytesAfter;
+    unsigned char* prop = nullptr;
+    int pid = -1;
 
     if (XGetWindowProperty(dpy, win, atomPID, 0, 1, False, XA_CARDINAL,
-        &actualType, &actualFormat, &nitems, &bytesAfter,
-        &prop) != Success ||
-        !prop) {
-        return -1;
-        }
-
-        int pid = (nitems > 0) ? *((unsigned long *)prop) : -1;
-    XFree(prop);
+                           &actualType, &actualFormat, &nItems, &bytesAfter,
+                           &prop) == Success &&
+        prop && nItems > 0) {
+        pid = *((unsigned long*)prop);
+        XFree(prop);
+    }
     return pid;
 }
 
-std::string getProcessName(int pid) {
-    if (pid <= 0)
-        return "(unknown)";
+
+std::string getProcName(int pid) {
+    if (pid <= 0) return "(unknown)";
     std::string path = "/proc/" + std::to_string(pid) + "/comm";
-    std::ifstream f(path);
-    if (!f)
-        return "(unknown)";
+    std::ifstream file(path);
+    if (!file.good()) return "(unknown)";
     std::string name;
-    std::getline(f, name);
+    std::getline(file, name);
     return name.empty() ? "(unknown)" : name;
 }
 
-struct WindowInfo {
-    std::string title;
-    int pid;
-    std::string proc;
-    int x, y, width, height;
-};
 
-std::vector<WindowInfo> listWindows(Display *dpy) {
-    std::vector<WindowInfo> out;
-    Atom atomClientList = XInternAtom(dpy, "_NET_CLIENT_LIST", True);
-    if (atomClientList == None)
-        return out;
+void getGeometry(Display* dpy, Window win,
+                 int& x, int& y, int& width, int& height) {
+    Window root = DefaultRootWindow(dpy);
+    XWindowAttributes attr;
+    if (!XGetWindowAttributes(dpy, win, &attr)) {
+        x = y = width = height = 0;
+        return;
+    }
 
-    Atom actualType;
-    int actualFormat;
-    unsigned long nitems, bytesAfter;
-    unsigned char *prop = nullptr;
-
-    if (XGetWindowProperty(dpy, DefaultRootWindow(dpy), atomClientList, 0,
-        (~0L), False, XA_WINDOW, &actualType, &actualFormat,
-                           &nitems, &bytesAfter, &prop) != Success ||
-                           !prop) {
-        return out;
-                           }
-
-                           Window *wins = reinterpret_cast<Window *>(prop);
-
-                           for (unsigned long i = 0; i < nitems; ++i) {
-                               Window w = wins[i];
-                               if (!isNormalWindow(dpy, w))
-                                   continue;
-
-                               std::string title = getTextProperty(dpy, w, "_NET_WM_NAME");
-                               if (title.empty())
-                                   title = getTextProperty(dpy, w, "WM_NAME");
-                               if (title.empty())
-                                   continue;
-
-                               XWindowAttributes attr;
-                               if (!XGetWindowAttributes(dpy, w, &attr))
-                                   continue;
-
-                               int absX = attr.x;
-                               int absY = attr.y;
-                               Window child;
-                               XTranslateCoordinates(dpy, w, DefaultRootWindow(dpy), 0, 0, &absX,
-                                                     &absY, &child);
-
-                               int pid = getPID(dpy, w);
-                               std::string proc = getProcessName(pid);
-
-                               out.push_back({title, pid, proc, absX, absY, attr.width, attr.height});
-                           }
-
-                           XFree(prop);
-                           return out;
+    Window child;
+    int absX = attr.x, absY = attr.y;
+    XTranslateCoordinates(dpy, win, root, 0, 0, &absX, &absY, &child);
+    x = absX;
+    y = absY;
+    width = attr.width;
+    height = attr.height;
 }
 
 
+std::vector<Window> getClientList(Display* dpy) {
+    std::vector<Window> list;
+    Atom atomList = XInternAtom(dpy, "_NET_CLIENT_LIST", True);
+    if (atomList == None) return list;
+
+    Atom type;
+    int fmt;
+    unsigned long nItems, bytesAfter;
+    unsigned char* prop = nullptr;
+
+    if (XGetWindowProperty(dpy, DefaultRootWindow(dpy), atomList, 0, (~0L),
+                           False, XA_WINDOW, &type, &fmt, &nItems, &bytesAfter,
+                           &prop) == Success &&
+        prop) {
+        Window* wins = reinterpret_cast<Window*>(prop);
+        for (unsigned long i = 0; i < nItems; i++)
+            list.push_back(wins[i]);
+        XFree(prop);
+    }
+    return list;
+}
+
 int main() {
-    Display *dpy = XOpenDisplay(nullptr);
+    Display* dpy = XOpenDisplay(nullptr);
     if (!dpy) {
         std::cerr << "Cannot open X display\n";
         return 1;
     }
 
-    auto windows = listWindows(dpy);
-    XCloseDisplay(dpy);
+    std::vector<Window> windows = getClientList(dpy);
+    std::map<int, int> pidCounts;
 
     Json::Value arr(Json::arrayValue);
-    for (const auto &w : windows) {
+
+    for (Window w : windows) {
+        if (!isNormalWindow(dpy, w))
+            continue;
+
+        int pid = getPID(dpy, w);
+        pidCounts[pid]++;
+
+        std::string title = getTextProperty(dpy, w, "_NET_WM_NAME");
+        if (title.empty())
+            title = getTextProperty(dpy, w, "WM_NAME");
+        if (title.empty())
+            continue;
+
+        int x = 0, y = 0, width = 0, height = 0;
+        getGeometry(dpy, w, x, y, width, height);
+
+        std::string proc = getProcName(pid);
+        int uid = pidCounts[pid];
+        std::string id = std::to_string(pid) + "@" + std::to_string(uid);
+
         Json::Value item;
-        // --- Info ---
-        item["Title"] = w.title;
-        item["PID"] = w.pid;
-        item["Process"] = w.proc;
-        // --- Dimensions ---
-        item["Width"] = w.width;
-        item["Height"] = w.height;
-        // --- Position ---
-        item["X"] = w.x;
-        item["Y"] = w.y;
+        item["Title"] = title;
+        item["PID"] = pid;
+        item["UID"] = uid;
+        item["ID"] = id;
+        item["Process"] = proc;
+        item["Width"] = width;
+        item["Height"] = height;
+        item["X"] = x;
+        item["Y"] = y;
 
         arr.append(item);
     }
@@ -185,5 +179,7 @@ int main() {
     Json::StreamWriterBuilder builder;
     builder["indentation"] = "  ";
     std::cout << Json::writeString(builder, arr) << std::endl;
+
+    XCloseDisplay(dpy);
     return 0;
 }
